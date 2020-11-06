96c1bdf088a4ae7bf388b7a8c7bdb4b47e069580
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-06 17:11:35 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -1,10 +1,9 @@
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int tempС = scan.nextInt();
-        double tempF = 1.8 * tempС + 32;
-        System.out.println("В градусах Фаренгейта: " + tempF);
+        double a = scan.nextInt();
+        double b =  a / 100.0 *115;
+        System.out.println(b);
     }
 }
