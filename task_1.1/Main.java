c5b4a9677ff51759a27de26c8ba29db25e534cb9
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-06 15:29:25 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       String name = "Владимир" ;
+       int age = 80;
+       String city = "Высоцк";
+        System.out.println(name +" "+ city +" "+ age);
+    }
+}
