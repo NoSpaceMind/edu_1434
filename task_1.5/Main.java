b799e35fbb83c95aa9bfb6fbb4ef6ea987f72dd7
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-06 17:10:24 +0300

    Update Main.java

--- task_1.5/Main.java
+++ task_1.5/Main.java
@@ -1,1 +1,9 @@
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int tempС = scan.nextInt();
+        double tempF = 1.8 * tempС + 32;
+        System.out.println("В градусах Фаренгейта: " + tempF);
+    }
+}
