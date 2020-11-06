8ebc9353c980d7838b0b006f38ad50cc2166be83
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-06 17:22:49 +0300

    Update Main.java

--- task_1.6/Main.java
+++ task_1.6/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        double a = scan.nextInt();
+        int a = scan.nextInt();
         double b =  a / 100.0 *115;
         System.out.println(b);
     }
