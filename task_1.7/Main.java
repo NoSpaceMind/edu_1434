19ab255c341a2fb58a3ef09d987ac97150b21a3e
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-08 00:39:49 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размеры вашего бассейна (a × b × c) ");
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        System.out.println(a * b * c * 1000);
+        System.out.println("Для наполнения бассейна необходимо: " + a * b * c * 1000 + " литров воды"); //  1м3 воды = 1000л
     }
 }
