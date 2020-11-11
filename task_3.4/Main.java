016765dc60dec7ac8fe2f9b3a4dc34a9be13bf61
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-11 05:11:46 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,15 @@
+/*
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+*/
+
+public class Main {
+
+    public static String min(int a, int b) {
+        if ((a + b) > 10) return "true";
+        else return "false";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(10, 1));
+    }
+}
