0b208dd0659127f77db54f5dd02aa8ca95f66f4d
NoSpaceMind <73994567+NoSpaceMind@users.noreply.github.com>
2020-11-12 16:02:15 +0300

    Create Main.java

---
+++ task_4.5/Main.java
@@ -0,0 +1,34 @@
+/*
+task_4.5
+Создать абстрактный класс Animal с характеристиками животного.
+Создать класс Horse который наследуется от Animal, в классе Horse реализовать метод public void run(){ System.out.println("Игого, я поскакал(а)"); }
+Создать класс Pegasus который наследуется от Horse, в классе Pegasus реализовать метод public void fly(){ System.out.println("Игого, я полетел(а)"); }
+Создать объект лошади и вызвать метод run();
+Создать объект пегаса и вызвать метод fly();
+
+ */
+
+
+public class Main{
+    public static void main(String[] args) {
+        Horse teodor = new Horse();
+        Pegasus atos = new Pegasus();
+        teodor.run();
+        atos.fly();
+    }
+}
+
+abstract class Animal{
+    private String type;
+    private String squad;
+    private int age;
+    private int weight;
+}
+
+class Horse extends Animal{
+    public void run(){ System.out.println("Игого, я поскакал(а)"); }
+}
+
+class Pegasus extends Animal{
+    public void fly(){ System.out.println("Игого, я полетел(а)"); }
+}
